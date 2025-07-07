c00a43bc2c755a3e7e2d2a3660a06dd50c93787d
Lucas Bastien <134368131+lucas-ige@users.noreply.github.com>
2025-07-07 12:13:55 +0200

    Implement the cmd-line arguments and option file parsing mechanisms
    Options specified in the option file (if present) have lower priority compared
    to options specified as command-line arguments.

--- compile/compile_WRF.py
+++ compile/compile_WRF.py
@@ -7,6 +7,8 @@ License: BSD 3-clause "new" or "revised" license (BSD-3-Clause).
 """
 
 import os
+import argparse
+import json
 import commons as cms
 
 def identify_host_platform():
@@ -22,12 +24,55 @@ def identify_host_platform():
         raise NotImplementedError("Unknown host platform: %s." % nodename)
     return platform
 
-def get_options(filepath):
-    opts = dict(
-        repo=cms.URL_WRFCHEMPOLAR,
+
+def get_argparser():
+    """Return command-line arguments parser."""
+    parser = argparse.ArgumentParser(
+        description="Compile WRF.",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
     )
+    parser.add_argument(
+        "--optfile",
+        help="File containing compiling options (JSON format).",
+        default=None,
+    )
+    parser.add_argument(
+        "--repository",
+        help="Git repository containing the WRF model code.",
+        default=cms.URL_WRFCHEMPOLAR,
+    )
+    parser.add_argument(
+        "--destination",
+        help="Directory that will host the installation.",
+        default="./WRF",
+    )
+    return parser
+
+
+def get_options():
+    """Get the installation options.
+
+    Command-line arguments have priority over options specified in the
+    (optional) option file.
+
+    """
+    parser = get_argparser()
+    args = parser.parse_args()
+    if args.optfile is not None:
+        with open(args.optfile) as f:
+            args_from_file = json.load(f)
+        if not isinstance(args_from_file, dict):
+            raise ValueError("Option file must represent a JSON dictionnary.")
+        for optname in args_from_file:
+            if optname not in args:
+                raise ValueError("Unknown option in file: %s." % optname)
+        parser.set_defaults(**args_from_file)
+        args = parser.parse_args()
+    return args
+
 
 if __name__ == "__main__":
 
     host = identify_host_platform()
     print("Host platform is: %s" % host)
+    print("Options:", get_options())
