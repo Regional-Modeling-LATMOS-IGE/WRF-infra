243b86cdc3d55001ddb1293ca60c6eff46572b3f
Lucas Bastien <134368131+lucas-ige@users.noreply.github.com>
2025-07-09 17:25:21 +0200

    Implement the git-clone and git-checkout in the WRF compile script

--- compile/compile_WRF.py
+++ compile/compile_WRF.py
@@ -7,8 +7,10 @@ License: BSD 3-clause "new" or "revised" license (BSD-3-Clause).
 """
 
 import os
+import os.path
 import argparse
 import json
+import subprocess
 import commons as cms
 
 def identify_host_platform():
@@ -42,15 +44,25 @@ def get_argparser():
         default=cms.URL_WRFCHEMPOLAR,
     )
     parser.add_argument(
+        "--commit",
+        help="Git commit to use.",
+        default="polar/main",
+    )
+    parser.add_argument(
         "--destination",
         help="Directory that will host the installation.",
         default="./WRF",
+    )
+    parser.add_argument(
+        "--git",
+        help="Git command (useful to use a non-default installation).",
+        default="git",
     )
     return parser
 
 
 def get_options():
-    """Get the installation options.
+    """Get and pre-process the installation options.
 
     Command-line arguments have priority over options specified in the
     (optional) option file.
@@ -68,11 +80,61 @@ def get_options():
                 raise ValueError("Unknown option in file: %s." % optname)
         parser.set_defaults(**args_from_file)
         args = parser.parse_args()
+    if args.repository.startswith("http://"):
+        raise ValueError("We do not allow http connections (not secure).")
+    if repo_is_local(args.repository):
+        args.repository = process_path(args.repository)
+    args.destination = process_path(args.destination)
     return args
+
+
+def repo_is_local(repository):
+    """Return True iff given repository address is local.
+
+    This function only looks at the format of the given character
+    string. Whether the repository is local or remote, this function does not
+    check if the repository exists or not.
+
+    """
+    return (not "@" in repository
+            and not repository.startswith("http://")
+            and not repository.startswith("https://"))
+
+
+def process_path(path):
+    """Return a (hopefully) unique absolute version of given path."""
+    return os.path.abspath(os.path.expanduser(path))
+
+
+def run(args, **kwargs):
+    """Run given command+arguments as a subprocess, exception if problem."""
+    out = subprocess.run(args, **kwargs)
+    if out.returncode != 0:
+        msg = "Command '%s' exited with non-zero return code." % " ".join(args)
+        raise RuntimeError(msg)
+
+
+def clone_and_checkout(opts):
+    """Clone the WRF code and checkout the required commit.
+
+    If the repository is local and if it is the same as the destination, then
+    no git-cloning is done.
+
+    """
+    clone_it = (not repo_is_local(opts.repository)
+                or opts.repository != opts.destination
+                or not os.path.lexists(opts.destination))
+    if clone_it:
+        run([opts.git, "clone", opts.repository, opts.destination])
+    run([opts.git, "checkout", opts.commit], cwd=opts.destination)
 
 
 if __name__ == "__main__":
 
     host = identify_host_platform()
     print("Host platform is: %s" % host)
-    print("Options:", get_options())
+
+    opts = get_options()
+    print("Options:", opts)
+
+    clone_and_checkout(opts)
